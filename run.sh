4881b40b001fce626ed37a8cc4457fbab2045d9c
ukyen <123098575+ukyen@users.noreply.github.com>
2023-02-07 15:50:19 +0000

    chore: remove identifiers

--- run.sh
+++ run.sh
@@ -9,6 +9,11 @@ DBName='<Database_Name>'
 DBUser='<Database_User>'
 DBPassword='<Database_Password>'
 DBSecretName='<Secret_Name_For_DB_Credential>'
+SecretArn='<Secret_Manager_ARN>'
+RouteTableIds='<Route_Table_Ids>'
+VpcId='<VPC_ID>'
+S3VPCEndpointServiceName='<S3_VPC_Endpoint_Service_Name>'
+SecretManagerVPCEndpointServiceName='<SM_VPC_Endpoint_Service_Name>'
 # Options: true/false
 IsProduction='true'
 FUNCTION_NAME='ReadIMDBFunction'
@@ -16,7 +21,9 @@ FUNCTION_NAME='ReadIMDBFunction'
 invoke_cmd="sam local invoke $FUNCTION_NAME -e events/event.json"
 parameter_overrides="DBSecretName=DBSecretName IsProduction=$IsProduction \
 BucketName=$BucketName LambdaSecurityGroup=$LambdaSecurityGroup LambdaSubnets=$LambdaSubnets \
-DBHost=$DBHost DBUser=$DBUser DBPassword=$DBPassword"
+DBHost=$DBHost DBUser=$DBUser DBPassword=$DBPassword SecretArn=$SecretArn \
+RouteTableIds=$RouteTableIds VpcId=$VpcId S3VPCEndpointServiceName=$S3VPCEndpointServiceName \
+SecretManagerVPCEndpointServiceName=$SecretManagerVPCEndpointServiceName"
 
 if [ $1 = "invoke" ]; then
   eval "$invoke_cmd --parameter-overrides $parameter_overrides"
@@ -29,5 +36,5 @@ fi
 
 if [ $1 = "deploy" ]; then
   sam build
-  sam deploy --parameter-overrides $parameter_overrides -no-confirm-changeset
+  sam deploy --parameter-overrides $parameter_overrides --no-confirm-changeset
 fi
