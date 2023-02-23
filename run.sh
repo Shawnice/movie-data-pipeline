40a975b93d6ccf9475066fcb83be86b4651a298f
ukyen <123098575+ukyen@users.noreply.github.com>
2023-02-23 12:25:29 +0000

    feat(template): add DB instance

--- run.sh
+++ run.sh
@@ -6,11 +6,9 @@ IsProduction='true'
 FUNCTION_NAME='ReadIMDBFunction'
 
 invoke_cmd="sam local invoke $FUNCTION_NAME -e events/event.json"
-parameter_overrides="DBSecretName=DBSecretName IsProduction=$IsProduction \
-BucketName=$BucketName LambdaSecurityGroup=$LambdaSecurityGroup LambdaSubnets=$LambdaSubnets \
-DBHost=$DBHost DBUser=$DBUser DBPassword=$DBPassword SecretArn=$SecretArn \
-RouteTableIds=$RouteTableIds VpcId=$VpcId S3VPCEndpointServiceName=$S3VPCEndpointServiceName \
-SecretManagerVPCEndpointServiceName=$SecretManagerVPCEndpointServiceName"
+parameter_overrides="IsProduction=$IsProduction BucketName=$BucketName \
+LambdaSecurityGroup=$LambdaSecurityGroup LambdaSubnets=$LambdaSubnets \
+DBUser=$DBUser DBPassword=$DBPassword RouteTableIds=$RouteTableIds VpcId=$VpcId "
 
 if [ $1 = "invoke" ]; then
   eval "$invoke_cmd --parameter-overrides $parameter_overrides"
